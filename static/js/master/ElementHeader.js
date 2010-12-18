abc7f1e38029aad071306b2a07c8548c74c217c3
spicavigo <yousuffauzan@gmail.com>
2010-12-18 15:42:46 +0530

    QUOTEBIN-20, 26, and 2

---
+++ static/js/master/ElementHeader.js
@@ -0,0 +1,27 @@
+$(function(){
+        $('.user_links a[href$="login"]').fancybox({
+            href: '#entercode_form'
+        });
+        $('.user_links a[href$="get_code"]').fancybox({
+            href: '#get_code_form'
+        });
+        $('#get_code_form').live('submit', function(e){
+           e.preventDefault();
+           var that = $('#get_code_form');
+           var data = $(that).serialize();
+           $.get(AJAX_URL, data, function(response){
+                if (response.success){
+                    $('#flash_messages').html('Your code is sent. Please check your email');
+                    $.fancybox.close();
+                    $('#flash_messages').fadeIn("slow", function(){
+                    setTimeout(function(){$("#flash_messages").fadeOut("slow");}, 2000);});
+                } else {
+                    $(that).find('.error').html(response.reason);
+                    $.fancybox.resize();
+                }
+                
+           });
+            return false;
+           
+        });
+    });
